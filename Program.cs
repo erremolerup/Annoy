ca0af78fb988f83d692fb5a7768590d9dcd4937b
saraglavin <hej@saraglavin.se>
2018-12-03 13:59:03 +0100

    Skrivit kommentarer på koden.

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ namespace Modul2
 {
     class Program
     {
+        // Dessa variabler behövs inte, för du har skapat lokala i main. 
         //STEG 1 definiera variabler du ska använda
         static string name = "";
         static int age;
@@ -16,21 +17,23 @@ namespace Modul2
         }
 
         //STEG 3 
-        public static void AskForUsersName()
+        public static void AskForUsersName() // Skulle göra en metod med ett namn som mer visar vad man gör. 
         {
 
             Console.Write("What is your name? ");
             string name = Console.ReadLine();
-            
+            // Lokal variabel. 
             Console.Write("How old are you? ");
             int age = int.Parse(Console.ReadLine());
 
             Console.Write("What is your favorite letter? ");
             char letter = char.Parse(Console.ReadLine()); 
-
+           
             Console.WriteLine();
             Console.WriteLine("Thank you!");
             Console.WriteLine();
+            
+            // Här kan du göra en ny metod som kallas ex. ReturningInput(). 
 
             Console.ForegroundColor = ConsoleColor.Magenta;
             Console.WriteLine("Your name is: " + name);
