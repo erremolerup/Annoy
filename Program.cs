38acb0833e8b172c3dc2ae9aec567f639462cd16
saraglavin <hej@saraglavin.se>
2018-12-03 14:08:56 +0100

    Busat lite

--- Program.cs
+++ Program.cs
@@ -13,34 +13,38 @@ namespace Modul2
         //STEG 2 definiera metoder som behövs
         static void Main(string[] args)
         {
-            AskForUsersName();
+            AskForInformation();
+            ReturnInformation();
         }
 
         //STEG 3 
-        public static void AskForUsersName() // Skulle göra en metod med ett namn som mer visar vad man gör. 
+        public static void AskForInformation() // Skulle göra en metod med ett namn som mer visar vad man gör. 
         {
 
             Console.Write("What is your name? ");
-            string name = Console.ReadLine();
+            name = Console.ReadLine();
             // Lokal variabel. 
             Console.Write("How old are you? ");
-            int age = int.Parse(Console.ReadLine());
+            age = int.Parse(Console.ReadLine());
 
             Console.Write("What is your favorite letter? ");
-            char letter = char.Parse(Console.ReadLine()); 
-           
+            letter = char.Parse(Console.ReadLine());
+
             Console.WriteLine();
             Console.WriteLine("Thank you!");
             Console.WriteLine();
-            
+
             // Här kan du göra en ny metod som kallas ex. ReturningInput(). 
 
-            Console.ForegroundColor = ConsoleColor.Magenta;
-            Console.WriteLine("Your name is: " + name);
-            Console.WriteLine("Your age is: " + age);
-            Console.WriteLine("Your favorite letter is: " + letter);
-            Console.WriteLine();
         }
+        public static void ReturnInformation()
+        {
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.Write("Your name is: " + name);
+            Console.WriteLine("Your age is: " + age);
+            Console.WriteLine("Your favorite letter is: " + age);
+            Console.WriteLine();
 
+        }
     }
 }
